11e606eee69854fd878310b1cc6bbf1ceb8fc3b3
412-08-kanyakron <54249@yupparaj.ac.th>
2026-09-01 10:57:25 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -48,14 +48,14 @@ def show_result_dialog(ans1, ans2):
     # ตรวจข้อ 3
     if u_ans3 == "pen":
         st.sccess("✅ ข้อ 3: ถูกต้อง" )
-        score +-=1
+        score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
     
     # ตรวจข้อ 4
     if u_ans4 == "google":
         st.sccess("✅ ข้อ 4: ถูกต้อง" )
-        score +-=1
+        score += 1
     else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 

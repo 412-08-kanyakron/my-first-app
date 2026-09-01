ab7b832d02907b595a1405b8c1cdc5e7de246fd9
412-08-kanyakron <54249@yupparaj.ac.th>
2026-09-01 10:56:10 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -46,18 +46,18 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ตรวจข้อ 3
-  if u_ans3 == "pen":
-      st.sccess("✅ ข้อ 3: ถูกต้อง" )
-      score +-=1
-  else:
-      st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
+    if u_ans3 == "pen":
+        st.sccess("✅ ข้อ 3: ถูกต้อง" )
+        score +-=1
+    else:
+        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
     
- # ตรวจข้อ 4
-  if u_ans4 == "google":
-      st.sccess("✅ ข้อ 4: ถูกต้อง" )
-      score +-=1
-  else:
-      st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
+    # ตรวจข้อ 4
+    if u_ans4 == "google":
+        st.sccess("✅ ข้อ 4: ถูกต้อง" )
+        score +-=1
+    else:
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
